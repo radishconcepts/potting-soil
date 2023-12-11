56e87ed43d160d679c1dda3b78b36f1f4d1b3f60
Herr von Cleaudtwyck <ik@dennisonline.nl>
2023-12-11 09:04:09 +0100

    Fixed the wrong isset check in the Singleton.

--- includes/classes/Traits/Singleton.php
+++ includes/classes/Traits/Singleton.php
@@ -7,7 +7,7 @@ trait Singleton {
 	private static array $instances = [];
 
 	final public static function get_instance(): self {
-		if ( static::$instances[ static::class ] === null ) {
+		if ( !isset( static::$instances[ static::class ] ) ) {
 			static::$instances[ static::class ] = new static();
 		}
 
